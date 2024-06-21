d9a3e7b89ca83a8e3148d13d6e7e4aad9cd2ff28
agrim <agrimraj2022@gmail.com>
2024-06-21 06:09:29 +0530

    final css

--- front/src/pages/About.jsx
+++ front/src/pages/About.jsx
@@ -7,22 +7,19 @@ export default function About() {
                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                    <div className="md:w-5/12 lg:w-5/12">
                        <img
-                           src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                           src="https://i.ibb.co/n1tzgR4/10172518-8294.jpg"
                            alt="image"
                        />
                    </div>
                    <div className="md:w-7/12 lg:w-6/12">
                        <h2 className="text-2xl text-green-900 font-bold md:text-4xl">
-                           React development is carried out by passionate developers
+                           About The Project
                        </h2>
-                       <p className="mt-6 text-green-600">
-                           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                           accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                           aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                       <p className="mt-6 text-#0a0a0a">
+                       This project, built using the MERN stack, incorporates various features including CRUD operations on data.
                        </p>
-                       <p className="mt-4 text-green-600">
-                           Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-                           Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+                       <p className="mt-4 text-#0a0a0a">
+                       The project is designed to segment trainee data into distinct categories: New Trainees, Current Trainees, and Past Trainees. This segmentation ensures comprehensive data capture at each stage (e.g., enrollment details, progress metrics, final assessments), supports targeted reporting and analysis, optimizes resource allocation and support efforts, and enhances overall user experience by facilitating streamlined data management and access based on the trainee's current status or phase in the program.
                        </p>
                    </div>
                </div>
