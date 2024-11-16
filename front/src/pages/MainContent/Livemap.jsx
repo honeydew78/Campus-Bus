81053b0c29e9d4fc53e1abd325b769672764d71e
agrim <agrimraj2022@gmail.com>
2024-11-16 17:11:55 +0530

    location feature added

--- front/src/pages/MainContent/Livemap.jsx
+++ front/src/pages/MainContent/Livemap.jsx
@@ -3,6 +3,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import io from 'socket.io-client';
 import 'leaflet/dist/leaflet.css';
+import img from '../../assets/location-pin.png';
 
 // Fix for broken leaflet icons
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
@@ -15,59 +16,15 @@ L.Icon.Default.mergeOptions({
   shadowUrl: markerShadow,
 });
 
-// Uncomment and configure this logic in the future for real-time tracking
-/*
-const socket = io('http://localhost:4000'); // Update this URL as needed
-if (navigator.geolocation) {
-  navigator.geolocation.watchPosition(
-    (position) => {
-      const { latitude, longitude } = position.coords;
-      socket.emit("send-location", { latitude, longitude });
-    },
-    (error) => {
-      console.log(error);
-    },
-    {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
-    }
-  );
-}
-*/
+// Custom icon using your imported image
+const customIcon = new L.Icon({
+  iconUrl: img,
+  iconSize: [40, 40], // Adjust size as needed
+  iconAnchor: [20, 40], // Adjust anchor point as needed (half width, full height)
+  popupAnchor: [0, -40], // Adjust popup anchor
+});
 
 const LiveMap = () => {
-  // Uncomment this logic in the future for real-time tracking
-  /*
-  const [markers, setMarkers] = useState({});
-
-  useEffect(() => {
-    // Handle receiving new locations
-    socket.on('receive-location', (data) => {
-      console.log(data);
-      const { id, latitude, longitude } = data;
-      setMarkers((prevMarkers) => ({
-        ...prevMarkers,
-        [id]: { latitude, longitude },
-      }));
-    });
-
-    // Handle user disconnection
-    socket.on('user-disconnected', (id) => {
-      setMarkers((prevMarkers) => {
-        const updatedMarkers = { ...prevMarkers };
-        delete updatedMarkers[id];
-        return updatedMarkers;
-      });
-    });
-
-    return () => {
-      socket.off('receive-location');
-      socket.off('user-disconnected');
-    };
-  }, []);
-  */
-
   // Hardcoded location
   const hardcodedLocation = { latitude: 25.4322, longitude: 81.7704 };
 
@@ -81,7 +38,10 @@ const LiveMap = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[hardcodedLocation.latitude, hardcodedLocation.longitude]}>
+      <Marker 
+        position={[hardcodedLocation.latitude, hardcodedLocation.longitude]} 
+        icon={customIcon}
+      >
         <Popup>
           Hardcoded Location: 25.4322° N, 81.7704° E
         </Popup>
@@ -89,7 +49,7 @@ const LiveMap = () => {
 
       {/* Uncomment this logic to show markers from real-time data */}
       {/* {Object.keys(markers).map((id) => (
-        <Marker key={id} position={[markers[id].latitude, markers[id].longitude]}>
+        <Marker key={id} position={[markers[id].latitude, markers[id].longitude]} icon={customIcon}>
           <Popup>Bus ID: {id}</Popup>
         </Marker>
       ))} */}
